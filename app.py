e467566fc48278f98a28a16f1702f1d3daef2bdb
01one <84787013+01one@users.noreply.github.com>
2024-06-01 06:13:36 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#more articles at programmerbee.com
+
 import re
 from flask import Flask, render_template, request, jsonify
 
@@ -8,34 +10,36 @@ existing_usernames = ["user1", "user2", "user3"]
 
 
 def is_valid_username(username):
-    # Double check
-    return re.match("^[a-zA-Z0-9_]*$", username) is not None
+	# Double check
+	return re.match("^[a-zA-Z0-9_]*$", username) is not None
 
 
 @app.route('/')
 def index():
-    return render_template('username-id-generate.html')
+	return render_template('username-id-generate.html')
 
 
 @app.route('/check_username', methods=['POST'])
 def check_username():
-    username = request.form['username']
-    if username in existing_usernames:
-        return jsonify({'message': 'Username already exists! Please choose a different username.'})
-    elif not is_valid_username(username):
-        return jsonify({'message': 'Username contains special characters. Only letters, numbers, and underscores are allowed.'})
-    else:
-        return jsonify({'message': 'Username is available!'})
+	username = request.form['username']
+	if username in existing_usernames:
+		return jsonify({'message': 'Username already exists! Please choose a different username.'})
+	elif not is_valid_username(username):
+		return jsonify({'message': 'Username contains special characters. Only letters, numbers, and underscores are allowed.'})
+	else:
+		return jsonify({'message': 'Username is available!'})
 
 
 @app.route('/create_profile', methods=['POST'])
 def create_profile():
-    username = request.form['username']
+	username = request.form['username']
+	if username not in existing_usernames:  # Corrected typo here
+		existing_usernames.append(username)
+		return  jsonify({'message': f"Welcome, {username}! Your profile has been created successfully."})
+	else:
+		return jsonify({'message': 'Username already exists! Please choose a different username.'})
 
-    # Add the new username to the list of existing usernames
-    existing_usernames.append(username)
-    return f"Welcome, {username}! Your profile has been created successfully."
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
